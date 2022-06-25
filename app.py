b0ea38458a6359761e31b53482be6b9d938767e0
woohoocheezy <woohoocheezy@gmail.com>
2022-06-25 12:00:02 +0900

    last test

--- app.py
+++ app.py
@@ -27,4 +27,4 @@ def result(): # a predicted result of the photo
             return render_template('winter.html')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080, debug = True)
+    app.run(host='0.0.0.0', port=5000, debug = False)
